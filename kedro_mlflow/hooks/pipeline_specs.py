88c3b2dc85fd8663e0f22e20d67667a8145689a6
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-04-11 20:09:20 +0200

    - add the abitlity to log a PipelineML instance as a Mlflow Model after pipeline run - move generate_kedro_command - add function to infer conda_env as much as possible

--- kedro_mlflow/hooks/pipeline_specs.py
+++ kedro_mlflow/hooks/pipeline_specs.py
@@ -1,6 +1,7 @@
 import mlflow
 import re
-from typing import Any, Dict
+from typing import Union, Dict, Any
+from pathlib import Path
 
 from kedro.hooks import hook_impl
 from kedro.io import DataCatalog
@@ -9,13 +10,25 @@ from kedro.pipeline.node import Node
 from kedro.versioning.journal import _git_sha
 
 from kedro_mlflow.context import get_mlflow_conf
-from kedro_mlflow.utils import generate_kedro_command
+from kedro_mlflow.utils import (_get_package_requirements,
+                                _get_project_globals)
+from kedro_mlflow.mlflow import KedroPipelineModel
+from kedro_mlflow.pipeline import PipelineML
 
-class MlflowPipelineSpecs:
+class MlflowPipelineHook:
+
+    def __init__(self,
+                 conda_env: Union[str, Path, Dict[str, Any]] = None, 
+                 model_name: Union[str, None] = "model"):
+        self.conda_env = _format_conda_env(conda_env)
+        self.model_name = model_name
 
     @hook_impl
     def before_pipeline_run(
-        self, run_params: Dict[str, Any], pipeline: Pipeline, catalog: DataCatalog
+        self,
+        run_params: Dict[str, Any],
+        pipeline: Pipeline,
+        catalog: DataCatalog
     ) -> None:
         """Hook to be invoked before a pipeline runs.
         Args:
@@ -56,7 +69,7 @@ class MlflowPipelineSpecs:
         # TODO : this does not take into account not committed files, so it 
         # does not ensure reproducibility. Define what to do.
         mlflow.set_tag("git_sha", _git_sha(run_params["project_path"]))
-        mlflow.set_tag("kedro_command", generate_kedro_command(tags=run_params["tags"], 
+        mlflow.set_tag("kedro_command", _generate_kedro_command(tags=run_params["tags"], 
                                                                node_names=run_params["node_names"],
                                                                from_nodes=run_params["from_nodes"],
                                                                to_nodes=run_params["to_nodes"],
@@ -64,10 +77,12 @@ class MlflowPipelineSpecs:
                                                                load_versions=run_params["load_versions"], 
                                                                pipeline_name=run_params["pipeline_name"]))
 
-
     @hook_impl
     def after_pipeline_run(
-        self, run_params: Dict[str, Any], pipeline: Pipeline, catalog: DataCatalog,
+        self,
+        run_params: Dict[str, Any],
+        pipeline: Pipeline,
+        catalog: DataCatalog,
     ) -> None:
         """Hook to be invoked after a pipeline runs.
         Args:
@@ -92,5 +107,84 @@ class MlflowPipelineSpecs:
             catalog: The ``DataCatalog`` used during the run.
         """
 
-        #Close the mlflow active run at the end of the pipeline to avoid interactions with further
+        if isinstance(pipeline, PipelineML):
+            pipeline_catalog = pipeline.extract_pipeline_catalog(catalog)
+            artifacts = {name: Path(dataset._filepath).resolve().as_uri()
+                         for name, dataset in pipeline_catalog._data_sets.items()
+                         if name != pipeline.model_input_name}
+            mlflow.pyfunc.log_model(artifact_path=self.model_name,
+                                    python_model=KedroPipelineModel(pipeline_ml=pipeline,
+                                                                    catalog=sub_catalog),
+                                    artifacts=artifacts,
+                                     conda_env=self.conda_env)
+        # Close the mlflow active run at the end of the pipeline to avoid interactions with further runs
         mlflow.end_run()
+
+def _generate_kedro_command(tags,
+                           node_names,
+                           from_nodes,
+                           to_nodes,
+                           from_inputs,
+                           load_versions,
+                           pipeline_name):
+    cdm_list = ["kedro","run"]
+    SEP = " "
+    if len(from_inputs) > 0:
+        cdm_list.append("--from-inputs" + SEP  + ",".join(from_inputs))
+    if len(from_nodes)>0:
+        cdm_list.append("--from-nodes" + SEP  + ",".join(from_nodes))
+    if len(to_nodes)>0:
+        cdm_list.append("--to-nodes" + SEP  + ",".join(to_nodes))
+    if len(node_names)>0:
+        cdm_list.append("--node" + SEP  + ",".join(node_names))
+    if pipeline_name:
+        cdm_list.append("--pipeline" + SEP  + pipeline_name)
+    if len(tags)>0:
+        # "tag" is the name of the command, "tags" the value in run_params
+        cdm_list.append("--tag" + SEP + ",".join(tags))
+    if len(load_versions)>0:
+        # "load_version" is the name of the command, "load_versions" the value in run_params
+        formatted_versions= ["k:v".format(k=k, v=v) for k, v in load_versions.items()]
+        cdm_list.append("--load-version" + SEP + ",".join(formatted_versions))
+    
+    kedro_cmd = " ".join(cdm_list)
+    return kedro_cmd
+
+
+def _format_conda_env(conda_env: Union[str, Path, Dict[str, Any]] = None) -> Dict[str, Any]:
+    """Best effort to get dependecies of the project.
+
+    Keyword Arguments:
+        conda_env {[type]} -- It can be either :
+            - a path to a "requirements.txt": In this case
+            the packages are parsed and a conda env with
+            your current python_version and these dependencies is returned
+            - a path to an "environment.yml" : data is loaded and used as they are
+            - a Dict : used as the environment
+            - None (default: {None}) : try to infer the dependencies base on current package name
+
+    Returns:
+        Dict[str, Any] -- [description]
+    """
+    python_version = ".".join([str(sys.version_info.major), str(sys.version_info.minor), str(sys.version_info.micro)])
+    if isinstance(conda_env, str):
+        conda_env = Path(conda_env)
+    if isinstance(conda_env, Path):
+        if conda_env.suffix in (".yml", ".yaml"):
+            with open(conda_env, mode="r") as file_handler:
+                conda_env = yaml.safe_load(file_handler)
+        elif conda_env.suffix in (".txt"):
+            with open(conda_env, mode="r") as file_handler:
+                dependencies = _parse_requirements(file_handler)
+            conda_env = {"python": python_version,
+                         "dependencies": dependencies}
+    else:
+        project_globals = _get_project_globals(Path().cwd())
+        try:
+            conda_env = {"python": python_version,
+                         "dependencies": _get_project_globals(project_globals["python_package"])}
+        except:
+            conda_env = {"python": python_version,
+                         "dependencies": [project_globals["python_package"]]}
+
+    return conda_env

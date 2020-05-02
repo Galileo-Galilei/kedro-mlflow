3472cd01705dca126d0486d32cc83ba4c6c403be
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-05-02 14:06:01 +0200

    remove support for python 3.8

--- setup.py
+++ setup.py
@@ -58,7 +58,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7"
     ],
 )

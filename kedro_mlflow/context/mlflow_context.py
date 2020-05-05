a34238d48495530fc709f5fdacfa9a955662e39b
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-05-05 22:05:02 +0200

    remove unused MlflowContext class

--- kedro_mlflow/context/mlflow_context.py
+++ kedro_mlflow/context/mlflow_context.py
@@ -1,23 +1,8 @@
 from pathlib import Path
 
 from kedro.config import ConfigLoader
-from kedro.context import KedroContext
 
 from kedro_mlflow.context.config import KedroMlflowConfig
-
-
-class KedroMlflowContext(KedroContext):
-    @property
-    def mlflow(self):
-        """Read only property for the mlflow configuration based on yml
-        """
-        return self._get_mlflow_properties()
-
-    def _get_mlflow_properties(self):
-        conf_mlflow = self.config_loader.get("mlflow*", "mlflow*/**")
-        mlflow_properties = KedroMlflowConfig(project_path=self.project_path)
-        mlflow_properties.from_dict(conf_mlflow)
-        return mlflow_properties
 
 
 # this could be a read-only property in the context

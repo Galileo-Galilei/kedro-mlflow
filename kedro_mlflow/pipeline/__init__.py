738b01490a08302b2df1b75b67df47403b7971ec
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-05-06 23:58:53 +0200

    enable import of custom errors

--- kedro_mlflow/pipeline/__init__.py
+++ kedro_mlflow/pipeline/__init__.py
@@ -1,2 +1,6 @@
 from .modular_pipeline_ml import pipeline_ml
-from .pipeline_ml import PipelineML
+from .pipeline_ml import (
+    KedroMlflowPipelineMLDatasetsError,
+    KedroMlflowPipelineMLInputsError,
+    PipelineML,
+)

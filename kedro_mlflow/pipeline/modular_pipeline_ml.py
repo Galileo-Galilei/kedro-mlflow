4cc27a6c6eb83396d571680c0b6aa441f261ead5
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-05-08 20:31:29 +0200

    linting

--- kedro_mlflow/pipeline/modular_pipeline_ml.py
+++ kedro_mlflow/pipeline/modular_pipeline_ml.py
@@ -1,16 +1,12 @@
-from pathlib import Path
-from typing import Any, Dict, Union
-
 from kedro.pipeline import Pipeline
 
 from .pipeline_ml import PipelineML
 
 
 def pipeline_ml(
-    training: Pipeline,
-    inference: Pipeline,
-    env: Union[str, Path, Dict[str, Any]] = None,
-    instance_name: str = None,
+    training: Pipeline, inference: Pipeline, input_name: str = None,
 ) -> PipelineML:
-    pipeline = PipelineML(nodes=training.nodes, inference=inference)
+    pipeline = PipelineML(
+        nodes=training.nodes, inference=inference, input_name=input_name
+    )
     return pipeline

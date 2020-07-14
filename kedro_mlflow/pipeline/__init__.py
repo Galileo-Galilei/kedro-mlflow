bf81e0d9addadc55cfcd73cef2ac534dd2be47a5
Galileo-Galilei <29451317+Galileo-Galilei@users.noreply.github.com>
2020-07-14 22:25:42 +0200

    Revert "enable import of custom errors"
    This reverts commit 738b01490a08302b2df1b75b67df47403b7971ec.

--- kedro_mlflow/pipeline/__init__.py
+++ kedro_mlflow/pipeline/__init__.py
@@ -1,6 +1,2 @@
 from .modular_pipeline_ml import pipeline_ml
-from .pipeline_ml import (
-    KedroMlflowPipelineMLDatasetsError,
-    KedroMlflowPipelineMLInputsError,
-    PipelineML,
-)
+from .pipeline_ml import PipelineML

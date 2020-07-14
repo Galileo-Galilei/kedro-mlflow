afe4eae1bed22971dae8b66b3dbdbdf3bf9df806
Galileo-Galilei <29451317+Galileo-Galilei@users.noreply.github.com>
2020-07-14 22:25:42 +0200

    Revert "remove support for python 3.8"
    This reverts commit d568cba15d1e47cc168b9c935c1c0e5f999f45ba.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )

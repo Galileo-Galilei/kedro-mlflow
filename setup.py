378b3a7e7b48283ff036e7d6e3a0facdc261a349
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-07-14 20:31:39 +0200

    prepare CI for deployment

--- setup.py
+++ setup.py
@@ -63,5 +63,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )

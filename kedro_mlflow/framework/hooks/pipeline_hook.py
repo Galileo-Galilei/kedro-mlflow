9b0eea9a19da49f63d343c5d595c252e401e4961
Galileo-Galilei <29451317+Galileo-Galilei@users.noreply.github.com>
2020-07-14 22:25:42 +0200

    Revert "fix spelling mistake in variable  name"
    This reverts commit c5b0f540d84c26132ccbe4d0957597e99902a5c8.

--- kedro_mlflow/framework/hooks/pipeline_hook.py
+++ kedro_mlflow/framework/hooks/pipeline_hook.py
@@ -134,27 +134,27 @@ class MlflowPipelineHook:
 def _generate_kedro_command(
     tags, node_names, from_nodes, to_nodes, from_inputs, load_versions, pipeline_name
 ):
-    cmd_list = ["kedro", "run"]
+    cdm_list = ["kedro", "run"]
     SEP = " "
     if len(from_inputs) > 0:
-        cmd_list.append("--from-inputs" + SEP + ",".join(from_inputs))
+        cdm_list.append("--from-inputs" + SEP + ",".join(from_inputs))
     if len(from_nodes) > 0:
-        cmd_list.append("--from-nodes" + SEP + ",".join(from_nodes))
+        cdm_list.append("--from-nodes" + SEP + ",".join(from_nodes))
     if len(to_nodes) > 0:
-        cmd_list.append("--to-nodes" + SEP + ",".join(to_nodes))
+        cdm_list.append("--to-nodes" + SEP + ",".join(to_nodes))
     if len(node_names) > 0:
-        cmd_list.append("--node" + SEP + ",".join(node_names))
+        cdm_list.append("--node" + SEP + ",".join(node_names))
     if pipeline_name:
-        cmd_list.append("--pipeline" + SEP + pipeline_name)
+        cdm_list.append("--pipeline" + SEP + pipeline_name)
     if len(tags) > 0:
         # "tag" is the name of the command, "tags" the value in run_params
-        cmd_list.append("--tag" + SEP + ",".join(tags))
+        cdm_list.append("--tag" + SEP + ",".join(tags))
     if len(load_versions) > 0:
         # "load_version" is the name of the command, "load_versions" the value in run_params
-        formatted_versions = [f"{k}:{v}" for k, v in load_versions.items()]
-        cmd_list.append("--load-version" + SEP + ",".join(formatted_versions))
+        formatted_versions = ["k:v".format(k=k, v=v) for k, v in load_versions.items()]
+        cdm_list.append("--load-version" + SEP + ",".join(formatted_versions))
 
-    kedro_cmd = " ".join(cmd_list)
+    kedro_cmd = " ".join(cdm_list)
     return kedro_cmd
 
 

c5b7521002c9c7f941d78590e37e0a6815f88ccb
Galileo-Galilei <29451317+Galileo-Galilei@users.noreply.github.com>
2020-07-14 22:25:42 +0200

    Revert "prepare CI for deployment"
    This reverts commit e0b42c8be608bb79f78f97179afbef32ff2b6f6d.

--- setup.py
+++ setup.py
@@ -63,6 +63,5 @@ setup(
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
 )

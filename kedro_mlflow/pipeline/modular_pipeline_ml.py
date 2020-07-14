de8db2ef1f897a19ffb2d1dff22c9f4ec44ac694
Galileo-Galilei <29451317+Galileo-Galilei@users.noreply.github.com>
2020-07-14 22:25:42 +0200

    Revert "linting"
    This reverts commit 4cc27a6c6eb83396d571680c0b6aa441f261ead5.

--- kedro_mlflow/pipeline/modular_pipeline_ml.py
+++ kedro_mlflow/pipeline/modular_pipeline_ml.py
@@ -1,12 +1,16 @@
+from pathlib import Path
+from typing import Any, Dict, Union
+
 from kedro.pipeline import Pipeline
 
 from .pipeline_ml import PipelineML
 
 
 def pipeline_ml(
-    training: Pipeline, inference: Pipeline, input_name: str = None,
+    training: Pipeline,
+    inference: Pipeline,
+    env: Union[str, Path, Dict[str, Any]] = None,
+    instance_name: str = None,
 ) -> PipelineML:
-    pipeline = PipelineML(
-        nodes=training.nodes, inference=inference, input_name=input_name
-    )
+    pipeline = PipelineML(nodes=training.nodes, inference=inference)
     return pipeline

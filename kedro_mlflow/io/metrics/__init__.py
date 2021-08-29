dbeb3f413cf7c2ba0b51f98f4cf37c0f9bf69f2b
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2021-08-29 22:27:23 +0200

    :sparkles: Create an MlflowMetricHistoryDataSet to enable logging metric evolution over time (#73)

--- kedro_mlflow/io/metrics/__init__.py
+++ kedro_mlflow/io/metrics/__init__.py
@@ -1,2 +1,3 @@
 from .mlflow_metric_dataset import MlflowMetricDataSet
+from .mlflow_metric_history_dataset import MlflowMetricHistoryDataSet
 from .mlflow_metrics_dataset import MlflowMetricsDataSet

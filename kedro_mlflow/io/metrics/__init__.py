5358a1ac2304c316fed1e4448281d3814340c53c
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2023-12-19 12:28:38 +0100

    :truck: Rename mlflow_metrics_history_dataset file for consistency(#502)
    Signed-off-by: Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>

--- kedro_mlflow/io/metrics/__init__.py
+++ kedro_mlflow/io/metrics/__init__.py
@@ -1,6 +1,6 @@
 from .mlflow_metric_dataset import MlflowMetricDataset
 from .mlflow_metric_history_dataset import MlflowMetricHistoryDataset
-from .mlflow_metrics_dataset import MlflowMetricsHistoryDataset
+from .mlflow_metrics_history_dataset import MlflowMetricsHistoryDataset
 
 __all__ = [
     "MlflowMetricDataset",

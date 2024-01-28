Galileo-Galilei/kedro-mlflow

commit 27c6b47946c147b610726730960a0d2f94992ff3

:arrow_up: Upgrade ruff, sphinx_rtd_theme, sphinx-click and scikit-learn

setup.py
@@ -39,9 +39,9 @@ setup(
     extras_require={
         "doc": [
             "sphinx>=4.5.0,<8.0.0",
-            "sphinx_rtd_theme>=1.0,<1.4",
+            "sphinx_rtd_theme>=1.0,<1.5",
             "sphinx-markdown-tables~=0.0.15",
-            "sphinx-click>=3.1,<5.1",
+            "sphinx-click>=3.1,<5.2",
             "sphinx_copybutton~=0.5.0",
             "myst-parser>=0.17.2,<2.1.0",
         ],
@@ -50,8 +50,8 @@ setup(
             "pytest-cov>=2.8.0, <5.0.0",
             "pytest-lazy-fixture>=0.6.0, <1.0.0",
             "pytest-mock>=3.1.0, <4.0.0",
-            "ruff==0.1.8",  # ensure consistency with pre-commit
-            "scikit-learn>=0.23.0, <1.4.0",
+            "ruff>==0.1.0,<0.2.0",  # ensure consistency with pre-commit
+            "scikit-learn>=0.23.0, <1.5.0",
             "kedro-datasets[pandas.CSVDataSet]",
         ],
         "dev": [

commit 4349137013263b09519c638a05314870f7613bf1

:bug: Fix extra_requires wrong formatting introduced by #520

setup.py
@@ -50,7 +50,7 @@ setup(
             "pytest-cov>=2.8.0, <5.0.0",
             "pytest-lazy-fixture>=0.6.0, <1.0.0",
             "pytest-mock>=3.1.0, <4.0.0",
-            "ruff>==0.1.0,<0.2.0",  # ensure consistency with pre-commit
+            "ruff>=0.1.0,<0.2.0",  # ensure consistency with pre-commit
             "scikit-learn>=0.23.0, <1.5.0",
             "kedro-datasets[pandas.CSVDataSet]",
         ],
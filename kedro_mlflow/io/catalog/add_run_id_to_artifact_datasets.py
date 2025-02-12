a20ea2aee1acccab89bcd4233496ba633dfa9b2d
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2025-02-12 22:23:03 +0100

    :bug: Enable to load an artifact from another run_id with ThreadRunner (#622)

--- kedro_mlflow/io/catalog/add_run_id_to_artifact_datasets.py
+++ kedro_mlflow/io/catalog/add_run_id_to_artifact_datasets.py
@@ -5,5 +5,5 @@ from kedro_mlflow.io.artifacts.mlflow_artifact_dataset import (
 
 def add_run_id_to_artifact_datasets(catalog, run_id: str):
     for name, dataset in catalog._datasets.items():
-        if _is_instance_mlflow_artifact_dataset(dataset):
+        if (_is_instance_mlflow_artifact_dataset(dataset)) and (dataset.run_id is None):
             catalog._datasets[name].run_id = run_id

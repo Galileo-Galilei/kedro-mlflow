8dd37c220b2eedddf2cf8420bea331b464d822cd
Yolan Honoré-Rougé <yolan.honore.rouge@gmail.com>
2025-02-16 23:45:58 +0100

    :coffin: Remove unused utils functions

--- kedro_mlflow/utils.py
+++ kedro_mlflow/utils.py
@@ -1,13 +1,5 @@
 from pathlib import Path
-from typing import Any, List, Union
-
-
-def _parse_requirements(path: Union[str, Path], encoding="utf-8") -> List:
-    with open(path, encoding=encoding) as file_handler:
-        requirements = [
-            x.strip() for x in file_handler if x.strip() and not x.startswith("-r")
-        ]
-    return requirements
+from typing import Any, Union
 
 
 def _is_project(project_path: Union[str, Path]) -> bool:

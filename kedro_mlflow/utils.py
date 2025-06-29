1ef88e5f0495332c1c97d8c3cb8118357f65840f
Yolan Honoré-Rougé <29451317+Galileo-Galilei@users.noreply.github.com>
2025-06-29 17:50:34 +0200

    update hook_impl arguments

--- kedro_mlflow/utils.py
+++ kedro_mlflow/utils.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Union
+from typing import Union
 
 
 def _is_project(project_path: Union[str, Path]) -> bool:
@@ -10,13 +10,3 @@ def _is_project(project_path: Union[str, Path]) -> bool:
         from kedro.utils import _is_project as _ip
 
     return _ip(project_path)
-
-
-def _find_kedro_project(current_dir: Path) -> Any:
-    try:
-        # untested in the CI, for retrocompatiblity with kedro >=0.19.0,<0.19.3
-        from kedro.framework.startup import _find_kedro_project as _fkp
-    except ImportError:
-        from kedro.utils import _find_kedro_project as _fkp
-
-    return _fkp(current_dir)
